b971ecad815461df3fd348c0be3a695706500186
BlockchainInhibitor <emilsgulbis8@gmail.com>
2024-12-01 14:59:04 +0200

    feat: DAY-1

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+mod days;
+
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: advent_of_code <day>");
+        std::process::exit(1);
+    }
+
+    let day = &args[1];
+    match day.as_str() {
+        "1-1" => days::day_1_1::run(),
+        "1-2" => days::day_1_2::run(),
+        _ => eprintln!("Day {} not implemented!", day),
+    }
+}
